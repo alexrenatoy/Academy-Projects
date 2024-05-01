5a9ffc5c3b2ad26524583af96b6389b01beb894d
migue1angel <macf06200@gmail.com>
2024-05-01 12:14:09 -0500

    archivos scss

--- rugrats/src/app/suppliers/supplier-form/supplier-form.component.ts
+++ rugrats/src/app/suppliers/supplier-form/supplier-form.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-supplier-form',
   templateUrl: './supplier-form.component.html',
-  styleUrl: './supplier-form.component.css'
+  styleUrl: './supplier-form.component.scss'
 })
 export class SupplierFormComponent {
 
--- rugrats/src/app/suppliers/supplier-list/supplier-list.component.ts
+++ rugrats/src/app/suppliers/supplier-list/supplier-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-supplier-list',
   templateUrl: './supplier-list.component.html',
-  styleUrl: './supplier-list.component.css'
+  styleUrl: './supplier-list.component.scss'
 })
 export class SupplierListComponent {
   list=[
