6ac200182ca7be07f27e73b185a50195c1325648
alexrenatoy <160887842+alexrenatoy@users.noreply.github.com>
2024-05-10 13:04:22 -0500

    reactive-forms

--- rugrats/src/app/auth/employees/register/register.component.ts
+++ rugrats/src/app/auth/employees/register/register.component.ts
@@ -13,19 +13,19 @@ export class RegisterComponent {
 
   constructor() {
     this.registerForm = this.buildForm;
-    console.log(this.registerForm)
+    console.log(this.registerForm.controls)
   }
 
 
   get buildForm() {
     return this.formBuilder.group({
-      name: ['', [ Validators.required,Validators.minLength(3) ]],
+      name: ['Andy', [ Validators.required,Validators.minLength(3) ]],
       lastName: ['', [ Validators.required,Validators.minLength(3) ]],
       email: ['', [ Validators.required,Validators.minLength(10) ]],
       phone: [0, [ Validators.required,Validators.minLength(10) ]],
       birthdate: [new Date(), [ Validators.required ]],
       gender: ['', [ Validators.required]],
-      notifyOffer: [false, [ Validators.required]],    
+      notifyOffer: [null, [ Validators.required]],    
     });
   }
 
@@ -33,9 +33,23 @@ export class RegisterComponent {
   get nameField(): AbstractControl {
   return this.registerForm.controls['name'];
 };
-
+  get lastNameField(): AbstractControl {
+    return this.registerForm.controls['lastName'];
+  };
+  get emailField(): AbstractControl {
+    return this.registerForm.controls['email'];
+  };
+  get phoneField(): AbstractControl {
+    return this.registerForm.controls['phone'];
+  };
   get birthdateField(): AbstractControl {
     return this.registerForm.controls['birthdate'];
+  };
+  get genderField(): AbstractControl {
+    return this.registerForm.controls['gender'];
+  };
+  get notifyOfferField(): AbstractControl {
+    return this.registerForm.controls['notifyOffer'];
   };
 
 
