6b818a056b1097fbf68020f2a1c0e6538f422a5e
alexrenatoy <160887842+alexrenatoy@users.noreply.github.com>
2024-05-11 22:05:38 -0500

    colorcitos

--- rugrats/src/app/auth/employees/register/register.component.ts
+++ rugrats/src/app/auth/employees/register/register.component.ts
@@ -19,10 +19,10 @@ export class RegisterComponent {
 
   get buildForm() {
     return this.formBuilder.group({
-      name: ['Andy', [ Validators.required,Validators.minLength(3),Validators.maxLength(15),Validators.pattern('[a-zA-Z ]*') ]],
+      name: ['', [ Validators.required,Validators.minLength(3),Validators.maxLength(15),Validators.pattern('[a-zA-Z ]*') ]],
       lastName: ['', [ Validators.required,Validators.minLength(3), Validators.maxLength(15),Validators.pattern('[a-zA-Z ]*') ]],
       email: ['', [ Validators.required,Validators.minLength(10),Validators.maxLength(35),Validators.email ]],
-      phone: [null, [ Validators.required,Validators.minLength(10),Validators.maxLength(10),Validators.pattern('^[0-9]*$') ]],
+      phone: ['', [ Validators.required,Validators.minLength(9),Validators.maxLength(10),Validators.pattern('^[0-9]*$') ]],
       birthdate: [new Date(), [ Validators.required ]],
       gender: ['', [ Validators.required]],
       notifyOffer: [false, [ Validators.requiredTrue]],    
@@ -56,7 +56,7 @@ export class RegisterComponent {
   validateForm(){
     if (this.registerForm.valid){
       for (let item of Object.values(this.registerForm.controls))
-          console.log(item.value)
+          console.log(item.value)        
     }else {
       alert('Error al registrarte')
     }
